7c3378c023872edfcc5f315e72349f175b3ea11e
friendy0 <friendy2021@outlook.com>
2023-04-12 23:03:38 +0800

    save

---
+++ exp5/train.py
@@ -0,0 +1,24 @@
+from gensim.models import word2vec
+import multiprocessing
+from gensim.models.word2vec import LineSentence
+import logging
+import os.path
+import sys
+
+program = os.path.basename(sys.argv[0])
+logger = logging.getLogger(program)
+#1.format: 指定输出的格式和内容，format可以输出很多有用信息，
+#%(asctime)s: 打印日志的时间
+#%(levelname)s: 打印日志级别名称
+#%(message)s: 打印日志信息
+# logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+# logging.root.setLevel(level=logging.INFO)
+# #打印这是一个通知日志
+# logger.info("running %s" % ' '.join(sys.argv))
+
+input_path_name = 'preprocessed.txt'  # inp:分好词的文本
+model = word2vec.Word2Vec(LineSentence(input_path_name),vector_size=256, window=5, min_count=5,workers=multiprocessing.cpu_count(),epochs=50)
+model.save('model.model')
+
+word_vectors = model.wv
+word_vectors.save("wordvectors.wordvectors")
